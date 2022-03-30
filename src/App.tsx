4c22bcb98f1f4c34b314c3046543a10695357fad
Yz4230 <yuzupon1480@gmail.com>
2022-03-30 12:59:03 +0900

    wip: impl push slide

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import { loremIpsum } from "lorem-ipsum";
 import { useState } from "react";
 import { animated, useSpring } from "react-spring";
 import useMeasure from "react-use-measure";
+
+const ToLeft = css({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  transform: "translateX(-100%)",
+});
+
+const ToRight = css({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  transform: "translateX(100%)",
+});
+
+const Center = css({
+  position: "relative",
+  transform: "translateX(0%)",
+});
+
+const Transision = css({
+  transition: "transform 1s ease-in-out",
+});
 
 const Container = styled.div({
   width: "100vw",
@@ -25,14 +49,13 @@ const PageContainer = styled(animated.div)({
   border: "1px solid #BDC3C7",
   borderRadius: "1em",
   overflow: "hidden",
-  padding: "1em",
 });
 
 const lorem = () => loremIpsum({ count: Math.ceil(Math.random() * 30) });
 
 function App() {
   const [text, setText] = useState(lorem);
-  const [overlapText] = useState(lorem);
+  const [overlapText, setOverlapText] = useState(lorem);
   const [ref, { height }] = useMeasure();
   const [started, setStarted] = useState(false);
 
@@ -42,57 +65,39 @@ function App() {
     },
   });
 
-  const toRightStyle = useSpring({
-    cancel: !started,
-    from: {
-      transform: "translateX(0%)",
-    },
-    to: {
-      transform: "translateX(100%)",
-    },
-  });
-
-  const fromLeftStyles = useSpring({
-    cancel: !started,
-    from: {
-      transform: "translateX(-100%)",
-    },
-    to: {
-      transform: "translateX(0%)",
-    },
-  });
-
-  const regenerateText = () => setText(lorem);
+  const regenerateText = () => {
+    setText(lorem);
+    setOverlapText(lorem);
+  };
 
   return (
     <Container>
       <Button onClick={regenerateText}>Regenerate text</Button>
-      <Button css={{ marginTop: "1em" }} onClick={() => setStarted(true)}>
-        Start
+      <Button css={{ marginTop: "1em" }} onClick={() => setStarted(!started)}>
+        {started ? "Stop" : "Start"}
       </Button>
       <PageContainer css={{ marginTop: "1em" }} style={styles}>
-        <div ref={ref} css={{ position: "relative", overflow: "hidden" }}>
-          <animated.p
-            css={{
-              display: "inline-block",
-              position: started ? "absolute" : "relative",
-            }}
-            style={toRightStyle}
+        <div ref={ref} css={{ position: "relative" }}>
+          <div
+            css={[
+              {
+                padding: "1em",
+              },
+              Transision,
+              started ? ToLeft : Center,
+            ]}
           >
             {text}
-          </animated.p>
-          <animated.p
-            css={{
-              display: "inline-block",
-              position: started ? "relative" : "absolute",
-              top: 0,
-              left: 0,
-              color: "red",
-            }}
-            style={fromLeftStyles}
+          </div>
+          <div
+            css={[
+              { color: "red", padding: "1em" },
+              Transision,
+              !started ? ToRight : Center,
+            ]}
           >
             {overlapText}
-          </animated.p>
+          </div>
         </div>
       </PageContainer>
     </Container>
