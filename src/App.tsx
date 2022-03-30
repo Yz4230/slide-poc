e62c8bdcdd34b6697eb402b029e472ef26ec4b6c
Yz4230 <yuzupon1480@gmail.com>
2022-03-30 11:36:02 +0900

    WIP: animate height

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import styled from "@emotion/styled";
-
-import logo from "./assets/logo.svg";
+import { loremIpsum } from "lorem-ipsum";
+import { useState } from "react";
+import { animated, useSpring } from "react-spring";
+import useMeasure from "react-use-measure";
 
 const Container = styled.div({
   width: "100vw",
-  height: "100vh",
+  minHeight: "100vh",
   display: "flex",
   flexFlow: "column",
   justifyContent: "center",
   alignItems: "center",
 });
 
-const Logo = styled.img({
-  width: "32em",
-  aspectRatio: "1/1",
+const Button = styled.button({
+  padding: "1em",
+  border: "none",
+  borderRadius: "0.5em",
+  cursor: "pointer",
 });
 
+const PageContainer = styled(animated.div)({
+  width: "40em",
+  border: "1px solid #BDC3C7",
+  borderRadius: "1em",
+  overflow: "hidden",
+  padding: "1em",
+});
+
+const lorem = () => loremIpsum({ count: Math.ceil(Math.random() * 30) });
+
 function App() {
+  const [text, setText] = useState(lorem);
+  const [ref, { height }] = useMeasure();
+
+  const styles = useSpring({
+    to: {
+      height: height > 0 ? `${height}px` : undefined,
+    },
+  });
+
+  const regenerateText = () => setText(lorem);
+
   return (
     <Container>
-      <h1>React Boilerplate</h1>
-      <Logo src={logo} alt="logo" />
+      <Button onClick={regenerateText}>Regenerate text</Button>
+      <PageContainer css={{ marginTop: "1em" }} style={styles}>
+        <div ref={ref}>
+          <p css={{ display: "inline-block" }}>{text}</p>
+        </div>
+      </PageContainer>
     </Container>
   );
 }
