ee47380827ced4fa013f753fa4784f3e80a6dec9
Yz4230 <yuzupon1480@gmail.com>
2022-03-30 23:33:57 +0900

    🎉 impl slide animation!

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,16 @@ import { useState } from "react";
 import { animated, useSpring } from "react-spring";
 import useMeasure from "react-use-measure";
 
-const ToLeft = css({
+const SLIDE_DURATION = 500;
+
+const Left = css({
   position: "absolute",
   top: 0,
   left: 0,
   transform: "translateX(-100%)",
 });
 
-const ToRight = css({
+const Right = css({
   position: "absolute",
   top: 0,
   left: 0,
@@ -25,8 +27,10 @@ const Center = css({
 });
 
 const Transision = css({
-  transition: "transform 1s ease-in-out",
+  transition: `transform ${SLIDE_DURATION}ms ease-in-out`,
 });
+
+type SlideDirection = "left" | "right";
 
 const Container = styled.div({
   width: "100vw",
@@ -51,53 +55,72 @@ const PageContainer = styled(animated.div)({
   overflow: "hidden",
 });
 
+const Page = styled("div")({
+  padding: "1em",
+});
+
 const lorem = () => loremIpsum({ count: Math.ceil(Math.random() * 30) });
 
 function App() {
-  const [text, setText] = useState(lorem);
-  const [overlapText, setOverlapText] = useState(lorem);
+  const [page, setPage] = useState(0);
+  const [data, setTexts] = useState(() =>
+    Array.from({ length: 5 }, () => lorem())
+  );
   const [ref, { height }] = useMeasure();
-  const [started, setStarted] = useState(false);
 
   const styles = useSpring({
     to: {
       height: height > 0 ? `${height}px` : undefined,
     },
+    config: {
+      duration: SLIDE_DURATION,
+    },
   });
 
   const regenerateText = () => {
-    setText(lorem);
-    setOverlapText(lorem);
+    setTexts(Array.from({ length: 5 }, () => lorem()));
+  };
+
+  const slide = (direction: SlideDirection) => {
+    setPage((page) => {
+      if (direction === "left") {
+        return Math.max(page - 1, 0);
+      }
+      return Math.min(page + 1, data.length - 1);
+    });
   };
 
   return (
     <Container>
       <Button onClick={regenerateText}>Regenerate text</Button>
-      <Button css={{ marginTop: "1em" }} onClick={() => setStarted(!started)}>
-        {started ? "Stop" : "Start"}
-      </Button>
+      <div
+        css={{
+          marginTop: "1em",
+          display: "flex",
+          alignItems: "center",
+          gap: "1em",
+        }}
+      >
+        <Button onClick={() => slide("left")}>Move Left</Button>
+        <div>{page}</div>
+        <Button onClick={() => slide("right")}>Move Right</Button>
+      </div>
       <PageContainer css={{ marginTop: "1em" }} style={styles}>
         <div ref={ref} css={{ position: "relative" }}>
-          <div
-            css={[
-              {
-                padding: "1em",
-              },
-              Transision,
-              started ? ToLeft : Center,
-            ]}
-          >
-            {text}
-          </div>
-          <div
-            css={[
-              { color: "red", padding: "1em" },
-              Transision,
-              !started ? ToRight : Center,
-            ]}
-          >
-            {overlapText}
-          </div>
+          {data.map((text, i) => (
+            <Page
+              key={text}
+              css={[
+                Math.abs(page - i) > 1 && { display: "none" },
+                Transision,
+                page === i && Center,
+                page === i + 1 && Left,
+                page === i - 1 && Right,
+              ]}
+            >
+              {`${i} ${text}`}
+            </Page>
+          ))}
         </div>
       </PageContainer>
     </Container>
